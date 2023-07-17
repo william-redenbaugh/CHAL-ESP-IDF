4c2d08e32b849d674f2692fba1e8a74292852002
William Personal <william-personal@Williams-MacBook-Pro.local>
2023-07-17 14:31:08 -0700

    LED strip i2s implemenation

--- chal_struct_definitions.h
+++ chal_struct_definitions.h
@@ -9,4 +9,24 @@ typedef struct os_i2s{
     i2s_port_t port;
 }os_i2s_host_t;
 
+typedef enum{
+    STRIP_NEOPIXEL_RGB
+}led_strip_type_t;
+
+#define ZERO_BUFFER_SIZE        (48)
+#define BYTES_PER_COLOR         (4)
+#define RGB_BYTES               (3)
+#define SAMPLE_RATE             (93750)
+
+typedef struct os_led_strip{
+    int bus; 
+    led_strip_type_t type;
+    int gpio;
+
+    size_t out_buffer_size;
+    uint8_t *out_buffer; 
+    uint8_t off_buffer[ZERO_BUFFER_SIZE];
+
+}os_led_strip_t;
+
 #endif
---
+++ os_led_strip.cpp
@@ -0,0 +1,79 @@
+#include "global_includes.h"
+
+#include <driver/i2s.h>
+i2s_config_t i2s_config = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+    .sample_rate = SAMPLE_RATE,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+    .channel_format = I2S_CHANNEL_FMT_RIGHT_LEFT,
+    .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+    .intr_alloc_flags = 0,
+    .dma_buf_count = 4,
+    .use_apll = false,
+};
+
+static const uint16_t bitpatterns[4] = {0x88, 0x8e, 0xe8, 0xee};
+
+
+int os_led_strip_init(os_led_strip_t *strip, led_strip_type_t type, int bus, int gpio, uint32_t numpixels){
+
+    if(strip == NULL){
+        return OS_RET_NULL_PTR;
+    }
+
+    i2s_pin_config_t pin_config = {.bck_io_num = -1,
+                               .ws_io_num = -1,
+                               .data_out_num = gpio,
+                               .data_in_num = -1};
+
+    strip->out_buffer_size = BYTES_PER_COLOR * RGB_BYTES * numpixels;
+    strip->out_buffer = (uint8_t*)malloc(sizeof(uint8_t) * strip->out_buffer_size);
+    i2s_config.dma_buf_len = strip->out_buffer_size;
+
+    i2s_driver_install((i2s_port_t)bus, &i2s_config, 0, NULL);
+    i2s_set_pin((i2s_port_t)bus, &pin_config);
+    return OS_RET_OK;
+}
+
+int os_led_strip_set(os_led_strip_t *strip, uint32_t pixel, uint8_t r, uint8_t g, uint8_t b){
+    
+    if(strip == NULL){
+        return OS_RET_NULL_PTR;
+    }
+
+    uint32_t index = pixel * BYTES_PER_COLOR * RGB_BYTES;
+    // Green
+    strip->out_buffer[index] = bitpatterns[g >> 6 & 0x03];
+    strip->out_buffer[index + 1] = bitpatterns[g >> 4 & 0x03];
+    strip->out_buffer[index + 2] = bitpatterns[g >> 2 & 0x03];
+    strip->out_buffer[index + 3] = bitpatterns[g & 0x03];
+
+    // Red bitwise manipulation
+    strip->out_buffer[index + 4] = bitpatterns[r >> 6 & 0x03];
+    strip->out_buffer[index + 5] = bitpatterns[r >> 4 & 0x03];
+    strip->out_buffer[index + 6] = bitpatterns[r >> 2 & 0x03];
+    strip->out_buffer[index + 7] = bitpatterns[r & 0x03];
+
+    // Blue bitwise manipulation
+    strip->out_buffer[index + 8] = bitpatterns[b >> 6 & 0x03];
+    strip->out_buffer[index + 9] = bitpatterns[b >> 4 & 0x03];
+    strip->out_buffer[index + 10] = bitpatterns[b >> 2 & 0x03];
+    strip->out_buffer[index + 11] = bitpatterns[b & 0x03];
+
+    return OS_RET_OK;
+}
+
+int os_led_strip_show(os_led_strip_t *strip){
+    
+    if(strip == NULL){
+        return OS_RET_NULL_PTR;
+    }
+
+    size_t bytes_written;
+    i2s_write((i2s_port_t)strip->bus, strip->out_buffer, strip->out_buffer_size, &bytes_written, portMAX_DELAY);
+    i2s_write((i2s_port_t)strip->bus, strip->off_buffer, sizeof(strip->off_buffer), &bytes_written, portMAX_DELAY);
+    os_thread_sleep_ms(10);
+
+    i2s_zero_dma_buffer((i2s_port_t)strip->bus);
+    return OS_RET_OK;
+}
