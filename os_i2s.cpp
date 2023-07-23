4a4995ebc7ecdc994e4f5ef79cde73283de25fa9
William Redenbaugh <wmredenbaugh@gmail.com>
2023-07-23 05:03:40 -0700

    debugging of i2s interface most complete

--- os_i2s.cpp
+++ os_i2s.cpp
@@ -1,6 +1,10 @@
 #include "platform_cshal.h"
 #include "global_includes.h"
 
+#define TOTAL_SAMPLES 256
+#define BYTES_PER_SAMPLE 4
+#define MAX_DMA_BUFFER_SIZE TOTAL_SAMPLES *BYTES_PER_SAMPLE
+
 static inline i2s_bits_per_sample_t map_bits_sample(os_i2s_sample_bits_t bits)
 {
     i2s_bits_per_sample_t bits_sample;
@@ -51,7 +55,7 @@ static inline i2s_channel_fmt_t map_channels(os_i2s_channels_t chan)
     return chn_fmt;
 }
 
-int i2s_host_init(os_i2s_host_t *host, int bus, os_i2s_channels_t chan, os_i2s_sample_bits_t bits, uint32_t sample_rate)
+int i2s_host_init(os_i2s_host_t *host, int bus, os_i2s_pinmap_t pinmap, os_i2s_channels_t chan, os_i2s_sample_bits_t bits, uint32_t sample_rate)
 {
 
     // Configure I2S peripheral
@@ -60,20 +64,33 @@ int i2s_host_init(os_i2s_host_t *host, int bus, os_i2s_channels_t chan, os_i2s_s
     // Configure channels
     i2s_channel_fmt_t chn_fmt = map_channels(chan);
 
+    // This generates a compiler warning... don't change it
     i2s_config_t i2s_config = {
         .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
         .sample_rate = sample_rate,
         .bits_per_sample = bits_sample,
         .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+        .communication_format = I2S_COMM_FORMAT_I2S,
         .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-        .dma_buf_count = 4,
-        .dma_buf_len = 1024,
+        .dma_buf_count = BYTES_PER_SAMPLE,
+        .dma_buf_len = MAX_DMA_BUFFER_SIZE,
         .use_apll = false,
         .tx_desc_auto_clear = false,
         .fixed_mclk = 0};
+    // I REPEAT, DON'T REMOVE .communication_format = I2S_COMM_FORMAT_I2S
+
+    host->port = (i2s_port_t)bus;
+    host->chip_select_gpio = pinmap.serial_data_cs;
+    host->pin_config.bck_io_num = pinmap.serial_clk;
+    host->pin_config.ws_io_num = pinmap.left_right_clk;
+    host->pin_config.data_out_num = I2S_PIN_NO_CHANGE;
+    host->pin_config.data_in_num = pinmap.serial_data;
 
     i2s_driver_install(host->port, &i2s_config, 0, NULL);
     i2s_set_pin(host->port, &host->pin_config);
+
+    pinMode(host->chip_select_gpio, OUTPUT);
+    digitalWrite(host->chip_select_gpio, LOW);
 
     return OS_RET_OK;
 }
