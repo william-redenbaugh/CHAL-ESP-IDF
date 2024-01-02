aded7418ced5a243d684413158a34d38a69941eb
William Redenbaugh <wmredenbaugh@gmail.com>
2024-01-01 21:05:49 -0800

    get unique id related to each device

---
+++ os_unique_id.cpp
@@ -0,0 +1,12 @@
+#include "global_includes.h"
+#include "esp_mac.h"
+#include "esp_efuse.h"
+
+uint64_t os_fetch_unique_id(void){
+    uint64_t value;
+    esp_read_mac((uint8_t*)&value, ESP_MAC_IEEE802154);
+}
+
+void burn_fuse(void){
+    esp_efuse_set_key_dis_write(EFUSE_BLK1);
+}
