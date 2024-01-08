88abd205eaf5056c5c6424296628310a01753c09
William Redenbaugh <wmredenbaugh@gmail.com>
2024-01-07 21:56:11 -0800

    updating mic to fix compiler warnings

--- os_i2s_mic.cpp
+++ os_i2s_mic.cpp
@@ -65,20 +65,21 @@ int i2s_host_init(os_i2s_host_t *host, int bus, os_i2s_pinmap_t pinmap, os_i2s_c
     i2s_channel_fmt_t chn_fmt = map_channels(chan);
 
     // This generates a compiler warning... don't change it
-    i2s_config_t i2s_config = {
-        .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
-        .sample_rate = sample_rate,
-        .bits_per_sample = bits_sample,
-        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-        .communication_format = I2S_COMM_FORMAT_I2S,
-        .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-        .dma_buf_count = BYTES_PER_SAMPLE,
-        .dma_buf_len = MAX_DMA_BUFFER_SIZE,
-        .use_apll = false,
-        .tx_desc_auto_clear = false,
-        .fixed_mclk = 0};
-    // I REPEAT, DON'T REMOVE .communication_format = I2S_COMM_FORMAT_I2S
+    i2s_config_t i2s_config = i2s_config_t();
+    i2s_config.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX);
+    i2s_config.sample_rate = sample_rate;
+    i2s_config.bits_per_sample = bits_sample;
+    i2s_config.channel_format = I2S_CHANNEL_FMT_ONLY_LEFT;
+    i2s_config.communication_format = I2S_COMM_FORMAT_STAND_I2S;
+    i2s_config.intr_alloc_flags = ESP_INTR_FLAG_LEVEL1;
+    i2s_config.dma_desc_num = BYTES_PER_SAMPLE;
+    i2s_config.dma_frame_num = MAX_DMA_BUFFER_SIZE;
+    i2s_config.use_apll = false;
+    i2s_config.tx_desc_auto_clear = false;
+    i2s_config.fixed_mclk = 0;
+    i2s_config.channel_format = chn_fmt;
 
+    // I REPEAT, DON'T REMOVE .communication_format = I2S_COMM_FORMAT_I2S
     host->cfg = i2s_config;
     host->port = (i2s_port_t)bus;
     host->chip_select_gpio = pinmap.serial_data_cs;
