fe5de2d0b53ab9a7bca66ae14fe868732778802e
William Redenbaugh <wmredenbaugh@gmail.com>
2025-05-28 22:48:38 -0700

    complete compile this for now

---
+++ os_pwm.cpp
@@ -0,0 +1,15 @@
+#include "global_includes.h"
+
+int os_pwm_gpio_init(int gpio, int freq, int resolution){
+
+    return OS_RET_OK;
+}
+
+int os_pwm_gpio_set(int gpio, int value){
+    return OS_RET_OK;
+
+}
+
+int os_pwm_gpio_uninit(int gpio){
+    return OS_RET_OK;
+}
